a6408005b00c172f35911c8e12f577cf2738ad02
Fernando Schulz <fasschulz@gmail.com>
2025-03-10 11:15:22 -0300

    First Migration

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -4,14 +4,16 @@ namespace FilmesAPI.Models;
 
 public class Filme
 {
+    [Key]
+    [Required]
     public int Id { get; set; }
 
     [Required(ErrorMessage = "O Título do filme é obrigatório")]
-    public string Titulo { get; set; }
+    public required string Titulo { get; set; }
 
     [Required(ErrorMessage = "O Gênero do filme é obrigatório")]
     [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
-    public string Genero { get; set; }
+    public required string Genero { get; set; }
 
     [Required(ErrorMessage = "A Duração do filme é obrigatória")]
     [Range(70, 600, ErrorMessage = "A Duração deve ter entre 70 e 600 minutos")]

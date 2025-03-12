c23adc648db14f23d7c65e333dbae5a1a8137b4b
Fernando Schulz <fasschulz@gmail.com>
2025-03-12 14:38:57 -0300

    Adicionando resource strings nas validacoes de model e dto

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using FilmesAPI.Resources;
+using System.ComponentModel.DataAnnotations;
 
 namespace FilmesAPI.Models;
 
@@ -8,14 +9,14 @@ public class Filme
     [Required]
     public int Id { get; set; }
 
-    [Required(ErrorMessage = "O Título do filme é obrigatório")]
+    [Required(ErrorMessageResourceName = "TituloRequired", ErrorMessageResourceType = typeof(FilmeStrings))]
     public required string Titulo { get; set; }
 
-    [Required(ErrorMessage = "O Gênero do filme é obrigatório")]
-    [MaxLength(50, ErrorMessage = "O tamanho do gênero não pode exceder 50 caracteres")]
+    [Required(ErrorMessageResourceName = "GeneroRequired", ErrorMessageResourceType = typeof(FilmeStrings))]
+    [MaxLength(50, ErrorMessageResourceName = "GeneroMaxLength", ErrorMessageResourceType = typeof(FilmeStrings))]
     public required string Genero { get; set; }
 
-    [Required(ErrorMessage = "A Duração do filme é obrigatória")]
-    [Range(70, 600, ErrorMessage = "A Duração deve ter entre 70 e 600 minutos")]
+    [Required(ErrorMessageResourceName = "DuracaoRequired", ErrorMessageResourceType = typeof(FilmeStrings))]
+    [Range(70, 600, ErrorMessageResourceName = "DuracaoRange", ErrorMessageResourceType = typeof(FilmeStrings))]
     public int Duracao { get; set; }    
 }

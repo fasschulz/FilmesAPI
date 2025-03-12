5e4de938f4d6872bb685e7e1b98c18e9524f4da4
Fernando Schulz <fasschulz@gmail.com>
2025-03-12 10:50:07 -0300

    Documentacao Swagger de todos os endpoints

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -36,6 +36,13 @@ public class FilmeController : ControllerBase
         return CreatedAtAction(nameof(RecuperaFilmePorId), new { id = filme.Id }, filme);
     }
 
+    /// <summary>
+    /// Retorna a lista de todos os filmes cadastrados no banco de dados
+    /// </summary>
+    /// <param name="skip">Quantos itens você deseja pular na paginação</param>
+    /// <param name="take">Quantos itens você deseja obter em cada página</param>
+    /// <returns>IEnumerable</returns>
+    /// <response code="200">Caso traga todos os itens com sucesso</response>
     [HttpGet]
     public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip = 0,
                                              [FromQuery] int take = 50)
@@ -44,6 +51,12 @@ public class FilmeController : ControllerBase
         return _mapper.Map<List<ReadFilmeDto>>(filmes);        
     }
 
+    /// <summary>
+    /// Retorna o filme de acordo com seu ID
+    /// </summary>
+    /// <param name="id">ID do filme que deseja obter</param>    
+    /// <returns>IActionResult</returns>
+    /// <response code="200">Caso traga o filme com sucesso</response>
     [HttpGet("{id}")]
     public IActionResult RecuperaFilmePorId(int id)
     {
@@ -55,6 +68,13 @@ public class FilmeController : ControllerBase
         return Ok(filmeDto);
     }
 
+    /// <summary>
+    /// Atualização completa de todos os campos de um filme
+    /// </summary>
+    /// <param name="id">ID do filme que deseja atualizar</param>
+    /// <param name="filmeDto">Objeto com os campos necessários para a atualização de um filme</param>
+    /// <returns>IActionResult</returns>
+    /// <response code="204">Caso atualize o filme com sucessso</response>
     [HttpPut("{id}")]
     public IActionResult AtualizaFilme(int id, [FromBody] UpdateFilmeDto filmeDto)
     {
@@ -68,6 +88,18 @@ public class FilmeController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>
+    /// Atualização parcial de campos específicos de um filme
+    /// </summary>
+    /// <param name="id">ID do filme que deseja atualizar</param>
+    /// <remarks>
+    /// <param name="patch">JSON com os campos necessários para a atualização parcial de um filme</param> \
+    /// op -> replace \
+    /// path -> indica qual campo de objeto quer atualizar \
+    /// value -> conteúdo que deseja atualizar no campo
+    /// </remarks>    
+    /// <returns>IActionResult</returns>
+    /// <response code="204">Caso atualize o filme com sucessso</response>
     [HttpPatch("{id}")]
     public IActionResult AtualizaFilmeParcial(int id, JsonPatchDocument<UpdateFilmeDto> patch)
     {
@@ -86,6 +118,12 @@ public class FilmeController : ControllerBase
         return NoContent();
     }
 
+    /// <summary>
+    /// Deleção completa de um filme no banco de dados
+    /// </summary>
+    /// <param name="id">ID do filme que deseja deletar</param>    
+    /// <returns>IActionResult</returns>
+    /// <response code="204">Caso delete o filme com sucessso</response>
     [HttpDelete("{id}")]
     public IActionResult DeletaFilme(int id)
     {

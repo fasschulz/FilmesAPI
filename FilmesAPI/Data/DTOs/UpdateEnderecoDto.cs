2d456de5445be6758facac2f723a954aaf3171e4
Fernando Schulz <fasschulz@gmail.com>
2025-03-14 10:44:23 -0300

    Adicionando conceito de endereco

---
+++ FilmesAPI/Data/DTOs/UpdateEnderecoDto.cs
@@ -0,0 +1,12 @@
+﻿using FilmesAPI.Resources;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.DTOs;
+
+public class UpdateEnderecoDto
+{
+    [Required(ErrorMessageResourceName = "LogradouroRequired", ErrorMessageResourceType = typeof(EnderecoStrings))]
+    public string Logradouro { get; set; }
+
+    public string Numero { get; set; }
+}
---
+++ FilmesAPI/Models/Endereco.cs
@@ -0,0 +1,16 @@
+﻿using FilmesAPI.Resources;
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+
+public class Endereco
+{
+    [Key]
+    [Required]
+    public int Id { get; set; }
+
+    [Required(ErrorMessageResourceName = "LogradouroRequired", ErrorMessageResourceType = typeof(EnderecoStrings))]
+    public string Logradouro { get; set; }
+
+    public string Numero { get; set; }
+}

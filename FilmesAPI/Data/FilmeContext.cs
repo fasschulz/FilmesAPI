fde9f19482a5ab4a62ed93ad027e7358bcbd1919
Fernando Schulz <fasschulz@gmail.com>
2025-03-14 16:11:35 -0300

    Utilizando tabela de sessao como relacionamento entre Filme e Cinema

--- FilmesAPI/Data/FilmeContext.cs
+++ FilmesAPI/Data/FilmeContext.cs
@@ -10,6 +10,24 @@ public class FilmeContext : DbContext
             
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Sessao>()
+            .HasKey(sessao => new { sessao.FilmeId, sessao.CinemaId });
+
+        modelBuilder.Entity<Sessao>()
+            .HasOne(sessao => sessao.Cinema)
+            .WithMany(cinema => cinema.Sessoes)
+            .HasForeignKey(sessao => sessao.CinemaId);
+
+        modelBuilder.Entity<Sessao>()
+            .HasOne(sessao => sessao.Filme)
+            .WithMany(filme => filme.Sessoes)
+            .HasForeignKey(sessao => sessao.FilmeId);
+
+        base.OnModelCreating(modelBuilder);
+    }
+
     public DbSet<Filme> Filmes { get; set; }
     public DbSet<Cinema> Cinemas { get; set; }
     public DbSet<Endereco> Enderecos { get; set; }

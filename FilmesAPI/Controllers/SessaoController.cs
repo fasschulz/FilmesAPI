fc50aa0b88f910d7df261142f55433543df81daa
Fernando Schulz <fasschulz@gmail.com>
2025-03-17 16:47:08 -0300

    Metodo para deletar sessao do filme

--- FilmesAPI/Controllers/SessaoController.cs
+++ FilmesAPI/Controllers/SessaoController.cs
@@ -6,51 +6,65 @@ using FilmesAPI.Models;
 using Microsoft.EntityFrameworkCore;
 using FilmesAPI.Profiles;
 
-namespace FilmesApi.Controllers
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SessaoController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class SessaoController : ControllerBase
+    private FilmeContext _context;
+    private IMapper _mapper;
+
+    public SessaoController(FilmeContext context, IMapper mapper)
     {
-        private FilmeContext _context;
-        private IMapper _mapper;
+        _context = context;
+        _mapper = mapper;
+    }
 
-        public SessaoController(FilmeContext context, IMapper mapper)
+    [HttpPost]
+    public IActionResult AdicionaSessao(CreateSessaoDto dto)
+    {
+        Sessao sessao = _mapper.Map<Sessao>(dto);
+        _context.Sessoes.Add(sessao);
+        _context.SaveChanges();
+
+        return CreatedAtAction(nameof(RecuperaSessoesPorId), 
+            new { filmeId = sessao.FilmeId, cinemaId = sessao.CinemaId }, dto);
+    }
+
+    [HttpGet]
+    public IEnumerable<ReadSessaoDto> RecuperaSessoes()
+    {
+        return _mapper.Map<List<ReadSessaoDto>>(_context.Sessoes.ToList());
+    }
+
+    [HttpGet("{filmeId}/{cinemaId}")]
+    public IActionResult RecuperaSessoesPorId(int filmeId, int cinemaId)
+    {
+        Sessao? sessao = _context.Sessoes.FirstOrDefault(sessao => 
+        sessao.FilmeId == filmeId && sessao.CinemaId == cinemaId);
+
+        if (sessao != null)
         {
-            _context = context;
-            _mapper = mapper;
+            ReadSessaoDto sessaoDto = _mapper.Map<ReadSessaoDto>(sessao);
+
+            return Ok(sessaoDto);
         }
+        return NotFound();
+    }
 
-        [HttpPost]
-        public IActionResult AdicionaSessao(CreateSessaoDto dto)
-        {
-            Sessao sessao = _mapper.Map<Sessao>(dto);
-            _context.Sessoes.Add(sessao);
-            _context.SaveChanges();
+    [HttpDelete("{filmeId}/{cinemaId}")]
+    public IActionResult DeletaSessao(int filmeId, int cinemaId)
+    {
+        var sessao = _context.Sessoes
+            .Where(s => s.FilmeId == filmeId && s.CinemaId == cinemaId)
+            .FirstOrDefault();
 
-            return CreatedAtAction(nameof(RecuperaSessoesPorId), 
-                new { filmeId = sessao.FilmeId, cinemaId = sessao.CinemaId }, dto);
-        }
-
-        [HttpGet]
-        public IEnumerable<ReadSessaoDto> RecuperaSessoes()
-        {
-            return _mapper.Map<List<ReadSessaoDto>>(_context.Sessoes.ToList());
-        }
-
-        [HttpGet("{filmeId}/{cinemaId}")]
-        public IActionResult RecuperaSessoesPorId(int filmeId, int cinemaId)
-        {
-            Sessao? sessao = _context.Sessoes.FirstOrDefault(sessao => 
-            sessao.FilmeId == filmeId && sessao.CinemaId == cinemaId);
-
-            if (sessao != null)
-            {
-                ReadSessaoDto sessaoDto = _mapper.Map<ReadSessaoDto>(sessao);
-
-                return Ok(sessaoDto);
-            }
+        if (sessao == null)
             return NotFound();
-        }
+
+        _context.Remove(sessao);
+        _context.SaveChanges();
+        return NoContent();
     }
 }
